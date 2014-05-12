696719eda30a327f9eb26bf62c8c79ea2bd69976
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2014-05-12 23:45:47 +0200

    view as thumbnails

--- resources/mediaflow-ng.js
+++ resources/mediaflow-ng.js
@@ -10,7 +10,7 @@ mediaflow.filter('sizeConverter', function () {
             precision = 1;
         }
         if (size == 0 || size == null) {
-            return "";
+            return "";w
         }
         else if (!isNaN(size)) {
             var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
@@ -43,13 +43,13 @@ mediaflow.controller('MediaFlowFieldCtrl', function ($scope, $http) {
     $scope.showMedia = false;
     $scope.media = [];
     $scope.testConnection = function () {
-        $http.get('/admin/mediaflow/check').success(function (result) { // FIXME
+        $http.get('/admin/mediaflow/check').success(function (result) { 
             $scope.connection = result;
         });
     };
     $scope.testConnection();
     $scope.getMedia = function () {
-        $http.get('/admin/mediaflow/media').success(function (result) { // FIXME
+        $http.get('/admin/mediaflow/media').success(function (result) { 
             $scope.media = result;
         });
     };
@@ -63,15 +63,16 @@ mediaflow.controller('MediaFlowFieldCtrl', function ($scope, $http) {
 mediaflow.controller('MediaFlowBrowseCtrl', function ($scope, $http) {
     $scope.connection = true;
     $scope.searchText = '';
+    $scope.view = 'list';
     $scope.media = [];
     $scope.testConnection = function () {
-        $http.get('/admin/mediaflow/check').success(function (result) { // FIXME
+        $http.get('/admin/mediaflow/check').success(function (result) { 
             $scope.connection = result;
         });
     };
     $scope.testConnection();
     $scope.getMedia = function () {
-        $http.get('/admin/mediaflow/media').success(function (result) { // FIXME
+        $http.get('/admin/mediaflow/media').success(function (result) { 
             $scope.media = result;
         });
     };
